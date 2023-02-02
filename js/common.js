d24dc284507035d1c5bf954d28c39855583cd96c
HyunWoo Park <dlgur73@gmail.com>
2023-02-02 17:24:43 +0900

    ver 1.2
    헤더 부분 전부 수정 ( 반응형, 픽스드 사용 수정 )

--- js/common.js
+++ js/common.js
@@ -10,17 +10,31 @@ $('#header .openbtn').on('click', function(){
     $(this).prevUntil('h1').toggleClass('on')
 })
 
-$('#nav .depth1 > li').on('mouseover mouseout', function(){
-    if($('html').hasClass('pc')) {
-        $(this).toggleClass('on')
+$('#nav .depth1 > li').hover(
+    function(){
+        if($('html').hasClass('pc')) {
+            $(this).find('.depth2').stop().fadeIn()
+        }
+    },
+    function(){
+        if($('html').hasClass('pc')) {
+            $(this).find('.depth2').stop().fadeOut()
+        }
     }
-})
+)
 
-$('#nav .depth2 > li').on('mouseover mouseout', function(){
-    if($('html').hasClass('pc')) {
-        $(this).toggleClass('on')
+$('#nav .depth2 > li').hover(
+    function(){
+        if($('html').hasClass('pc')) {
+            $(this).find('.depth3').stop().fadeIn()
+        }
+    },
+    function(){
+        if($('html').hasClass('pc')) {
+            $(this).find('.depth3').stop().fadeOut()
+        }
     }
-})
+)
 
 $('#header #nav .depth1 > li > a').on('click', function(){
     if($('html').hasClass('mobile')) {
